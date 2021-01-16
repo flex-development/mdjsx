ef1a5c7cde4b2b3e23ed9466b3bc207f8145ab9b
Lexus Drumgold <lex@flexdevelopment.llc>
2021-01-16 14:59:12 -0500

    chore(mdjsx): add fixtures for jsx pragma + core lib fn options

---
+++ packages/mdjsx/__test__/fixtures/jsx-pragma.ts
@@ -0,0 +1,6 @@
+/**
+ * @file Fixtures - JSX Pragma
+ * @module test/fixtures/jsx-pragma
+ */
+
+export default '/* @jsxRuntime classic */\n/* @jsx mdx */\n'
---
+++ packages/mdjsx/__test__/fixtures/options.ts
@@ -0,0 +1,9 @@
+/**
+ * @file Fixtures - Options Argument
+ * @module test/fixtures/options
+ */
+
+export default {
+  buble: { objectAssign: 'Object.assign' },
+  mdx: { skipExport: true }
+}
